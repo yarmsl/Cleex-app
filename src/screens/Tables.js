28dbd22ef4a1429fef82e2ea47df000fc27fd312
yarmsl <slideryo@gmail.com>
2021-08-04 18:00:54 +0500

    firebase init

--- src/screens/Tables.js
+++ src/screens/Tables.js
@@ -39,72 +39,72 @@ const Tables = () => {
 	};
 
 	const idNotif = Math.floor(Math.random() * 1000);
-	// Clear badge number at start
-	PushNotification.getApplicationIconBadgeNumber(function (number) {
-		if (number > 0) {
-			PushNotification.setApplicationIconBadgeNumber(0);
-		}
-	});
+	// // Clear badge number at start
+	// PushNotification.getApplicationIconBadgeNumber(function (number) {
+	// 	if (number > 0) {
+	// 		PushNotification.setApplicationIconBadgeNumber(0);
+	// 	}
+	// });
 
-	PushNotification.getChannels(function (channels) {
-		console.log(channels);
-	});
+	// PushNotification.getChannels(function (channels) {
+	// 	console.log(channels);
+	// });
 
-	function createDefaultChannels() {
-		PushNotification.createChannel(
-			{
-				channelId: "default-channel-id", // (required)
-				channelName: `Default channel`, // (required)
-				channelDescription: "A default channel", // (optional) default: undefined.
-				soundName: "default", // (optional) See `soundName` parameter of `localNotification` function
-				importance: Importance.HIGH, // (optional) default: Importance.HIGH. Int value of the Android notification importance
-				vibrate: true, // (optional) default: true. Creates the default vibration pattern if true.
-			},
-			(created) => console.log(`createChannel 'default-channel-id' returned '${created}'`) // (optional) callback returns whether the channel was created, false means it already existed.
-		);
-	};
+	// function createDefaultChannels() {
+	// 	PushNotification.createChannel(
+	// 		{
+	// 			channelId: "default-channel-id", // (required)
+	// 			channelName: `Default channel`, // (required)
+	// 			channelDescription: "A default channel", // (optional) default: undefined.
+	// 			soundName: "default", // (optional) See `soundName` parameter of `localNotification` function
+	// 			importance: Importance.HIGH, // (optional) default: Importance.HIGH. Int value of the Android notification importance
+	// 			vibrate: true, // (optional) default: true. Creates the default vibration pattern if true.
+	// 		},
+	// 		(created) => console.log(`createChannel 'default-channel-id' returned '${created}'`) // (optional) callback returns whether the channel was created, false means it already existed.
+	// 	);
+	// };
 
-	function popInitialNotification() {
-		PushNotification.popInitialNotification((notification) => console.log('InitialNotication:', notification));
-	}
+	// function popInitialNotification() {
+	// 	PushNotification.popInitialNotification((notification) => console.log('InitialNotication:', notification));
+	// }
 
-	function localNotif(soundName) {
-		PushNotification.localNotification({
-			/* Android Only Properties */
-			channelId: soundName ? 'sound-channel-id' : 'default-channel-id',
-			ticker: 'My Notification Ticker', // (optional)
-			autoCancel: true, // (optional) default: true
-			largeIcon: 'ic_launcher', // (optional) default: "ic_launcher"
-			smallIcon: 'ic_notification', // (optional) default: "ic_notification" with fallback for "ic_launcher"
-			bigText: 'My big text that will be shown when notification is expanded', // (optional) default: "message" prop
-			subText: 'This is a subText', // (optional) default: none
-			color: 'red', // (optional) default: system default
-			vibrate: true, // (optional) default: true
-			vibration: 300, // vibration length in milliseconds, ignored if vibrate=false, default: 1000
-			tag: 'some_tag', // (optional) add tag to message
-			group: 'group', // (optional) add group to message
-			groupSummary: false, // (optional) set this notification to be the group summary for a group of notifications, default: false
-			ongoing: false, // (optional) set whether this is an "ongoing" notification
-			actions: ['Yes', 'No'], // (Android only) See the doc for notification actions to know more
-			invokeApp: true, // (optional) This enable click on actions to bring back the application to foreground or stay in background, default: true
+	// function localNotif(soundName) {
+	// 	PushNotification.localNotification({
+	// 		/* Android Only Properties */
+	// 		channelId: soundName ? 'sound-channel-id' : 'default-channel-id',
+	// 		ticker: 'My Notification Ticker', // (optional)
+	// 		autoCancel: true, // (optional) default: true
+	// 		largeIcon: 'ic_launcher', // (optional) default: "ic_launcher"
+	// 		smallIcon: 'ic_notification', // (optional) default: "ic_notification" with fallback for "ic_launcher"
+	// 		bigText: 'My big text that will be shown when notification is expanded', // (optional) default: "message" prop
+	// 		subText: 'This is a subText', // (optional) default: none
+	// 		color: 'red', // (optional) default: system default
+	// 		vibrate: true, // (optional) default: true
+	// 		vibration: 300, // vibration length in milliseconds, ignored if vibrate=false, default: 1000
+	// 		tag: 'some_tag', // (optional) add tag to message
+	// 		group: 'group', // (optional) add group to message
+	// 		groupSummary: false, // (optional) set this notification to be the group summary for a group of notifications, default: false
+	// 		ongoing: false, // (optional) set whether this is an "ongoing" notification
+	// 		actions: ['Yes', 'No'], // (Android only) See the doc for notification actions to know more
+	// 		invokeApp: true, // (optional) This enable click on actions to bring back the application to foreground or stay in background, default: true
 
-			when: null, // (optionnal) Add a timestamp pertaining to the notification (usually the time the event occurred). For apps targeting Build.VERSION_CODES.N and above, this time is not shown anymore by default and must be opted into by using `showWhen`, default: null.
-			usesChronometer: false, // (optional) Show the `when` field as a stopwatch. Instead of presenting `when` as a timestamp, the notification will show an automatically updating display of the minutes and seconds since when. Useful when showing an elapsed time (like an ongoing phone call), default: false.
-			timeoutAfter: null, // (optional) Specifies a duration in milliseconds after which this notification should be canceled, if it is not already canceled, default: null
+	// 		when: null, // (optionnal) Add a timestamp pertaining to the notification (usually the time the event occurred). For apps targeting Build.VERSION_CODES.N and above, this time is not shown anymore by default and must be opted into by using `showWhen`, default: null.
+	// 		usesChronometer: false, // (optional) Show the `when` field as a stopwatch. Instead of presenting `when` as a timestamp, the notification will show an automatically updating display of the minutes and seconds since when. Useful when showing an elapsed time (like an ongoing phone call), default: false.
+	// 		timeoutAfter: null, // (optional) Specifies a duration in milliseconds after which this notification should be canceled, if it is not already canceled, default: null
 
-			/* iOS only properties */
-			category: '', // (optional) default: empty string
-			subtitle: "Hello from Cleex", // (optional) smaller title below notification title
+	// 		/* iOS only properties */
+	// 		category: '', // (optional) default: empty string
+	// 		subtitle: "Hello from Cleex", // (optional) smaller title below notification title
 
-			/* iOS and Android properties */
-			id: idNotif, // (optional) Valid unique 32 bit integer specified as string. default: Autogenerated Unique ID
-			title: 'Cleex Push', // (optional)
-			message: 'Cleex test push', // (required)
-			playSound: !!soundName, // (optional) default: true
-			soundName: soundName ? soundName : 'default', // (optional) Sound to play when the notification is shown. Value of 'default' plays the default sound. It can be set to a custom sound such as 'android.resource://com.xyz/raw/my_sound'. It will look for the 'my_sound' audio file in 'res/raw' directory and play it. default: 'default' (default sound is played)
-			number: 10, // (optional) Valid 32 bit integer specified as string. default: none (Cannot be zero)
-		});
-	}
+	// 		/* iOS and Android properties */
+	// 		id: idNotif, // (optional) Valid unique 32 bit integer specified as string. default: Autogenerated Unique ID
+	// 		title: 'Cleex Push', // (optional)
+	// 		message: 'Cleex test push', // (required)
+	// 		playSound: !!soundName, // (optional) default: true
+	// 		soundName: soundName ? soundName : 'default', // (optional) Sound to play when the notification is shown. Value of 'default' plays the default sound. It can be set to a custom sound such as 'android.resource://com.xyz/raw/my_sound'. It will look for the 'my_sound' audio file in 'res/raw' directory and play it. default: 'default' (default sound is played)
+	// 		number: 10, // (optional) Valid 32 bit integer specified as string. default: none (Cannot be zero)
+	// 	});
+	// }
 
 	return (
 		<ScrollView style={styles.container}>
@@ -156,7 +156,7 @@ const Tables = () => {
 					title='Create default channel'
 					buttonStyle={theme.button.outlined}
 					titleStyle={theme.button.outlinedTitle}
-					onPress={() => createDefaultChannels()}
+					// onPress={() => createDefaultChannels()}
 				/>
 			</View>
 			<View style={styles.buttonBox}>
@@ -164,13 +164,13 @@ const Tables = () => {
 					title='Init Notif'
 					buttonStyle={theme.button.outlined}
 					titleStyle={theme.button.outlinedTitle}
-					onPress={() => popInitialNotification()}
+					// onPress={() => popInitialNotification()}
 				/>
 				<Button
 					title='Local Notif'
 					buttonStyle={theme.button.outlined}
 					titleStyle={theme.button.outlinedTitle}
-					onPress={() => localNotif()}
+					// onPress={() => localNotif()}
 				/>
 			</View>
 		</ScrollView>
